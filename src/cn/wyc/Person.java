1b55bd0a51a171952e04b9b6535d70aeb730f340
BG343093 <18758038180@163.com>
2018-04-24 20:18:30 +0800

    Test1

--- src/cn/wyc/Person.java
+++ src/cn/wyc/Person.java
@@ -1,5 +1,18 @@
 package cn.wyc;
 
+import org.junit.jupiter.api.Test;
+
 public class Person {
+
+    public void run(){
+        System.out.println("run");
+    }
+
+    public void eat(){
+        System.out.println("eat");
+    }
+
+
+
 
 }
---
+++ test/cn/wyc/PersonTest.java
@@ -0,0 +1,12 @@
+package cn.wyc;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+    @Test
+    public void test_run(){
+        Person person = new Person();
+        person.run();
+    }
+}
